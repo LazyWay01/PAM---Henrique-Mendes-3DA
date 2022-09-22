2400dc1c8d7ecb2e90fcb093d75c3d83714d6d13
Henrique Mendes <108155800+HenriqueMendes7@users.noreply.github.com>
2022-09-22 17:32:57 -0300

    Atualização no projeto

--- onepunchman/App.js
+++ onepunchman/App.js
@@ -1,32 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-
-const array = [
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Blast.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Tatsumaki.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Silver%20Fang.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Samurai%20Atomico.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Child%20Emperor.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/BofoyAnime.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/King.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Zombieman.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Drive%20Knight.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Pig%20God.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Darkshine.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Watchdog%20Man.png?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Flash%20Flashy.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Genos.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Tank-Top%20Master.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Metal%20Bat.jpg?raw=true',
-  'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Puri%20Puri%20Prisioner.jpg?raw=true',
-
-]; //criando tabela
-
-const logo = {
-  uri: 'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Blast.jpg?raw=true',
-  width: 64,
-  height: 64,
-};
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
 
 const styles = StyleSheet.create({
   div: {
@@ -43,6 +16,14 @@ const styles = StyleSheet.create({
   menu: {
     padding: 20,
     backgroundColor: '#000',
+  },
+
+  menuCenter: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   tituloMenu: {
     textAlign: 'center',
@@ -62,149 +43,107 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 5,
     display: 'flexBlock',
-    width: 300,
+    width: 500,
   },
   item: {
     padding: 10,
     paddingLeft: 0,
     fontSize: 25,
     height: 44,
+    fontWeight: 700,
     color: '#fff',
   },
   imagem: {
-    width: 100,
-    height: 100,
+    width: 250,
+    height: 350,
     borderRadius: 5,
   },
-  btn: {
+  imagemCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#444',
+    margin: 5,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
     color: '#fff',
-    marginLeft: 10,
-    padding: 7,
-    fontSize: 18,
-    backgroundColor: '#222222',
-    borderRadius: 5,
-    textAlign: 'center',
   },
 });
 
-const App = () => {
-  return (
-    <View style={styles.div}>
+export default class App extends Component<{}> {
+ render() {
+
+   var imagens = [
+    ['https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Blast.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Tatsumaki.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Silver%20Fang.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Samurai%20Atomico.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Child%20Emperor.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/BofoyAnime.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/King.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Zombieman.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Drive%20Knight.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Pig%20God.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Darkshine.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Watchdog%20Man.png?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Flash%20Flashy.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Genos.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Tank-Top%20Master.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Metal%20Bat.jpg?raw=true',
+    'https://github.com/HenriqueMendes7/PAM---Henrique-Mendes-3DA/blob/main/onepunchman/assets/img/Puri%20Puri%20Prisioner.jpg?raw=true'],
+    ['1. Blast', '2. Tatsumaki', '3. Silver Fang', '4. Atomic Samurai', '5. Child Emperor', '6. Metal Knight', '7. King', '8. Zombieman', '9. Drive Knight', '10. Pig God', '11. Darkshine', '12. Watchdog Man', '13. Flash Flashy', '14. Genos', '15. Tank-Top Master', '16. Metal Bat', '17. Puri-Puri Prisoner',]
+   ];
+    
+   return (
+     <View style={styles.div}>
       <Text style={styles.menu}>
+        <Text style={styles.menuCenter}>
         <Text style={styles.tituloMenu}>One Punch Man</Text>
-        {'\n'}
-        {'\n'}
-        <Text style={styles.btn}>Herois</Text>
-        <Text style={styles.btn}>Monstros</Text>
-        <Text style={styles.btn}>Classe S</Text>
+        </Text>
+        <Text style={styles.menuCenter}>
+        <Pressable style={styles.button}>
+          <Text style={styles.text}>Heróis</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.text}>Monstros</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.text}>Classe S</Text>
+        </Pressable>
+        </Text>
       </Text>
 
       <Text style={styles.titulo}>Classe S</Text>
+
       <Text style={styles.section}>
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={logo} />
-          {'\n'}
-          <Text style={styles.item}>1. Blast</Text>
-        </Text>
 
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[1]} />
-          {'\n'}
-          <Text style={styles.item}>2. Tatsumaki</Text>
-        </Text>
+          {imagens[1,0].map((item, key) => {
+            return (
+              <Text style={styles.card}>
+              <Text style={styles.imagemCard}>
+                <Image style={styles.imagem} source={item} />
+                {'\n'}
+                <Text style={styles.item} key={key}>{imagens[1][key]}</Text>
+              </Text>
+              </Text>
+            );
+          })}
 
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[2]} />
-          {'\n'}
-          <Text style={styles.item}>3. Silver Fang</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[3]} />
-          {'\n'}
-          <Text style={styles.item}>4. Atomic Samurai</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[4]} />
-          {'\n'}
-          <Text style={styles.item}>5. Child Emperor</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[5]} />
-          {'\n'}
-          <Text style={styles.item}>6. Metal Knight</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[6]} />
-          {'\n'}
-          <Text style={styles.item}>7. King</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[7]} />
-          {'\n'}
-          <Text style={styles.item}>8. Zombieman</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[8]} />
-          {'\n'}
-          <Text style={styles.item}>9. Drive Knight</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[9]} />
-          {'\n'}
-          <Text style={styles.item}>10. Pig God</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[10]} />
-          {'\n'}
-          <Text style={styles.item}>11. Darkshine</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[11]} />
-          {'\n'}
-          <Text style={styles.item}>12. Watchdog Man</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[12]} />
-          {'\n'}
-          <Text style={styles.item}>13. Flash Flashy</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[13]} />
-          {'\n'}
-          <Text style={styles.item}>14. Genos</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[14]} />
-          {'\n'}
-          <Text style={styles.item}>15. Tank-Top Master</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[15]} />
-          {'\n'}
-          <Text style={styles.item}>16. Metal Bat</Text>
-        </Text>
-
-        <Text style={styles.card}>
-          <Image style={styles.imagem} source={array[16]} />
-          {'\n'}
-          <Text style={styles.item}>17. Puri-Puri Prisoner</Text>
-        </Text>
+          
       </Text>
     </View>
-  );
-};
-
-export default App;
+   );
+ }
+}
